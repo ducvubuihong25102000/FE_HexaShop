c8e5166413a011d8474bf38e19c522e65ba79620
Duc toc dai <ducvubuihong25102000@gmail.com>
2022-04-03 21:16:19 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import './App.css';
+
+// import { Route, Router, Switch } from 'react-router-dom';
+
+import About from './pages/about/About';
+import Home from './pages/home/Home';
+import HomeTemplate from './templates/homeTemplate/HomeTemplate';
+import React from 'react';
+import { createBrowserHistory } from 'history';
+import { Route, Router, Switch } from 'react-router';
+import Login from 'pages/user/Login/Login';
+import SignUp from 'pages/user/SignUp/SignUp';
+import DetailTemplate from 'templates/detailTemPlate/DetailTemplate';
+import Detail from 'pages/detail/Detail';
+import ProductTemplate from 'templates/Products/ProductTemplate';
+import Productlist from 'pages/Products/Productlist';
+
+export const history = createBrowserHistory();
+
+function App() {
+  return (
+    <Router history={history}>
+      <Switch>
+        <HomeTemplate exact path="/" Component={Home}></HomeTemplate>
+        <HomeTemplate exact path="/about" Component={About}></HomeTemplate>
+        <DetailTemplate exact path="/detail" Component={Detail}></DetailTemplate>
+        <ProductTemplate exact path="/products" Component={Productlist}></ProductTemplate>
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={SignUp} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/templates/detailTemPlate/DetailTemplate.tsx
@@ -0,0 +1,29 @@
+import Footer from '../layouts/footer/Footer';
+
+import Header from '../layouts/header/Header';
+import { Route } from 'react-router';
+
+interface IpropsTemPlate {
+  Component: any;
+  path: string;
+  exact: boolean;
+}
+
+export default function DetailTemplate(props: IpropsTemPlate) {
+  let { Component, ...resRoute } = props;
+  return (
+    <Route
+      {...resRoute}
+      render={(propsRoute: object) => {
+        return (
+          <>
+            <Header></Header>
+            <Component {...propsRoute} />
+
+            <Footer></Footer>
+          </>
+        );
+      }}
+    />
+  );
+}
